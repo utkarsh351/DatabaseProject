42e40ddbc94cecfc6dcdc5bef05730160575addc
rashik <rashikbhasin@gmail.com>
2018-11-07 17:01:43 -0500

    minor change

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class MainApp {
 	
@@ -12,19 +13,30 @@ public class MainApp {
 			
 			connection connObject = new connection();
 			
+//			******************************************
 //			query like this everywhere
 //			connObject.createTable("CREATE TABLE TRYING1 " + "(COF_NAME VARCHAR(32), SUP_ID INTEGER, "
 //					+ "PRICE FLOAT, SALES INTEGER, TOTAL INTEGER)");
 //			
 //			connObject.insertQuery("INSERT INTO TRYING1 " + "VALUES ('Colombian', 101, 7.99, 0, 0)");
 			
-			rs = connObject.selectQuery("SELECT COF_NAME, PRICE FROM TRYING1");
-
-			while (rs.next()) {
-				String s = rs.getString("COF_NAME");
-				float n = rs.getFloat("PRICE");
-				System.out.println(s + "   " + n);
-			}
+//			rs = connObject.selectQuery("SELECT COF_NAME, PRICE FROM TRYING1");
+//
+//			while (rs.next()) {
+//				String s = rs.getString("COF_NAME");
+//				float n = rs.getFloat("PRICE");
+//				System.out.println(s + "   " + n);
+//			}
+//			******************************************
+			Scanner s = new Scanner(System.in);
+			
+			System.out.println("Welcome!!");
+			System.out.println("Enter user name:");
+			String username = s.nextLine();
+			System.out.println("Enter password:");
+			String password = s.nextLine();
+			
+//			now do stuff
 			
 		} catch (Throwable e) {
 			e.printStackTrace();
